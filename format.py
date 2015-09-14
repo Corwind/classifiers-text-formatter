21b34968577d8e82e51ce1be23b478eae58f8d0c
Guillaume Doré <corwin@my-little-pony.eu>
2015-09-14 12:15:50 +0200

    tf-idf computation

--- format.py
+++ format.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
 import sys
+from math import log10
 from collections import OrderedDict
 from string import punctuation
+from string import digits
 
 
 def clean_string(s):
     for c in punctuation:
         s = s.replace(c, " ")
+    for c in digits:
+        s = s.replace(c, "")
     return s
 
 
-def read_file(f):
+def read_file(f_in):
     lines = []
     with open(f_in, "r") as f:
         lines = f.readlines()
@@ -31,22 +35,31 @@ def build_dic(lines):
             if w.isalpha():
                 dic[w] = 0
                 dic.move_to_end(w)
-        return dic
+    return dic
 
 
 def format_data(f_out, lines, dic):
+    grades = []
+    clean_lines = []
+    for l in lines:
+        l = l.split("\t")
+        if len(l) == 1:
+            continue
+        grade = l[0]
+        grades.append("+1 " if grade[0] == "+" else "-1 ")
+        l = l[1:][0]
+        l = clean_string(l)
+        clean_lines.append(l)
+    idf = compute_idf(clean_lines, dic)
     with open(f_out, "w") as fout:
-        for l in lines:
-            l = l.split("\t")
-            if len(l) == 1:
-                continue
-            grade = l[0]
-            l = l[1:][0]
-            fout.write("+1 " if grade[0] == "+" else "-1 ")
-            l = clean_string(l)
-            for w in l.split():
-                if w.isalpha():
-                    dic[w] += 1
+        for j in range(len(clean_lines)):
+            for word in clean_lines[j].split():
+                tf = compute_tf(word, clean_lines[j])
+                try:
+                    dic[word] = tf * idf[word]
+                except:
+                    continue
+            fout.write(grades[j])
             i = 1
             for key, value in dic.items():
                 if(value != 0):
@@ -80,6 +93,29 @@ def read_dic(f_dict):
                 dic[w] = 0
                 dic.move_to_end(w)
     return dic
+
+
+def compute_tf(word, line):
+    i = 0
+    words = line.split()
+    for w in words:
+        if w == word:
+            i += 1
+    tf = i / len(words)
+    return tf
+
+
+def compute_idf(lines, dic):
+    idf = OrderedDict()
+    for key in dic.keys():
+        i = 0
+        for line in lines:
+            if key in line.split():
+                i += 1
+        if i == 0:
+            continue
+        idf[key] = log10(float(len(line)) / float(i))
+    return idf
 
 
 if __name__ == "__main__":
